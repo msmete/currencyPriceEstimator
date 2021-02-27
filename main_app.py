a2e2ef61a0447528cbaa1d9d84c8f11459651f50
veribranchusr <h2so4k@gmail.com>
2021-02-27 18:24:43 +0300

    First successfull prediction

--- main_app.py
+++ main_app.py
@@ -11,13 +11,12 @@ training_set_scaled, sc_predict = initializer.initialize()
 X_train, y_train = preprocess.train_test_split(training_set_scaled)
 predictor = model.create(90, training_set_scaled.shape[1]-1)
 predictor, history = train.train(predictor, X_train, y_train )
-predictions_future, predictions_train, datelist_future = predict.predict(predictor, X_train, datelist, n_future=90, n_past=90)
+predictions_future, predictions_train, datelist_future = predict.predict(predictor, X_train, datelist, n_future=90, n_past=60)
 # Inverse the predictions to original measurements
 
 
 y_pred_future = sc_predict.inverse_transform(predictions_future)
 y_pred_train = sc_predict.inverse_transform(predictions_train)
-
 
 
 PREDICTIONS_FUTURE = pd.DataFrame(y_pred_future, columns=[0]).set_index(pd.Series(datelist_future))
@@ -26,4 +25,4 @@ PREDICTION_TRAIN = pd.DataFrame(y_pred_train, columns=[0]).set_index(pd.Series(d
 # Convert <datetime.date> to <Timestamp> for PREDCITION_TRAIN
 PREDICTION_TRAIN.index = PREDICTION_TRAIN.index.to_series().apply(util.datetime_to_timestamp)
 
-print(PREDICTION_TRAIN.head(3))
+print(PREDICTION_TRAIN)
--- predict_util/predict.py
+++ predict_util/predict.py
@@ -5,11 +5,6 @@ def predict(model, X_train, datelist_train, n_future=90, n_past=60):
     # Generate list of sequence of days for predictions
     datelist_future = pd.date_range(datelist_train[-1], periods=n_future, freq='1d').tolist()
 
-    '''
-    Remeber, we have datelist_train from begining.
-    '''
-
-    # Convert Pandas Timestamp to Datetime object (for transformation) --> FUTURE
     datelist_future_ = []
     for this_timestamp in datelist_future:
         datelist_future_.append(this_timestamp.date())
